925274d11e0c64740f06dabaf47341352bbdf318
Dennis Agulo <dennisagulo@gmail.com>
2019-02-20 11:31:13 +0800

    Updated API routes

--- routes/api.php
+++ routes/api.php
@@ -5,8 +5,11 @@ use Illuminate\Http\Request;
 
 //Route::group([ 'middleware' => ['cors'], 'prefix' => '/v1' , 'namespace' => 'API\V1' ], function () {
 Route::group([ 'prefix' => '/v1' , 'namespace' => 'API\V1' ], function () {
-    Route::post( 'register', 'ApiAuthController@register' );
-    Route::post( 'login', 'ApiAuthController@login' );
-    Route::post( 'email/check', 'ApiAuthController@checkEmail' );
+    Route::group([ 'prefix' => 'auth'  ], function () {
+        Route::post( 'register', 'ApiAuthController@register' );
+        Route::post( 'login', 'ApiAuthController@login' );
+        Route::post( 'email/check', 'ApiAuthController@checkEmail' );
+    });
+
 });
 
