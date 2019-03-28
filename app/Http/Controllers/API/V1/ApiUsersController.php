523bb651c44883435be4b99b10f610a905afe37d
remz <remz.fernandez@appetiser.com.au>
2019-03-28 16:48:49 +0800

    [UPDATE] update api doc controller

--- app/Http/Controllers/API/V1/ApiUsersController.php
+++ app/Http/Controllers/API/V1/ApiUsersController.php
@@ -211,6 +211,59 @@ class ApiUsersController extends ApiBaseController
         return $this->apiSuccessResponse([ 'user'=>$user ], true, 'Profile Photo Uploaded Successfully ', self::HTTP_STATUS_REQUEST_OK);
     }
 
+    /**
+     * @OA\Post(
+     *      path="/user/work/experience",
+     *      tags={"User"},
+     *      summary="Add a work experience",
+     *      security={{"BearerAuth":{}}},
+     *      @OA\RequestBody(
+     *          required=true,
+     *          @OA\MediaType(
+     *              mediaType="application/x-www-form-urlencoded",
+     *              @OA\Schema(
+     *                  type="object",
+     *                  @OA\Property(
+     *                      property="job_role",
+     *                      description="<b>Required</b> Job Role",
+     *                      type="string",
+     *                      example="Developer"
+     *                  ),
+     *          @OA\Property(
+     *                      property="company_name",
+     *                      description="<b>Required</b> Company",
+     *                      type="string",
+     *                      example="Appetiser"
+     *                  ),
+     *              ),
+     *          ),
+     *      ),
+     *      @OA\Response(
+     *          response=400,
+     *          description="Invalid Token"
+     *      ),
+     *      @OA\Response(
+     *          response=401,
+     *          description="Token Expired"
+     *      ),
+     *      @OA\Response(
+     *          response=404,
+     *          description="Token Not Found"
+     *      ),
+     *      @OA\Response(
+     *          response=422,
+     *          description="Bad Request"
+     *      ),
+     *      @OA\Response(
+     *          response=500,
+     *          description="Internal Server Error"
+     *      ),
+     *      @OA\Response(
+     *          response=200,
+     *          description="Request OK"
+     *      )
+     * )
+     */
     public function addWorkExperience( Request $request )
     {
 
