1d84b5c4e4b982b4cb1a9b641da3cf2f6be64ee5
Jamielyn Monique Oropesa <jam.oropesa@appetiser.com.au>
2019-04-17 15:33:22 +0800

    modify alert info bar

--- public/js/app.js
+++ public/js/app.js
@@ -2470,7 +2470,7 @@ function _asyncToGenerator(fn) { return function () { var self = this, args = ar
     Bus.$on('alertVerify', function (email) {
       component.message = 'Please verify your account. You may check your confirmation email or click ';
       component.alert = 'alert-main alert-info alert-dismissible';
-      component.icon = '/img/icons/alert-info.png';
+      component.icon = '';
       component.resend = true;
       component.input_resend.email = email;
     });
