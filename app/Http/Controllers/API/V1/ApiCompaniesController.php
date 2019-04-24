d481dae7504b8c9c83fbb22e8e4d5fc80f17a7e4
remz <remz.fernandez@appetiser.com.au>
2019-04-24 11:56:58 +0800

    ADD company search

--- app/Http/Controllers/API/V1/ApiCompaniesController.php
+++ app/Http/Controllers/API/V1/ApiCompaniesController.php
@@ -3,11 +3,79 @@
 namespace App\Http\Controllers\API\V1;
 
 use App\Models\Companies\Company;
+use App\Repositories\CompanyRepository;
 use Illuminate\Http\Request;
 use JWTAuth;
 
 class ApiCompaniesController extends ApiBaseController
 {
+
+    /**
+     * @var CompanyRepository
+     */
+    protected $repository;
+
+    public function __construct(CompanyRepository $repository) {
+
+        $this->repository = $repository;
+    }
+
+    /**
+     * @OA\Get(
+     *      path="/company/search",
+     *      tags={"Company"},
+     *      summary="Search Company",
+     *      security={{"BearerAuth":{}}},
+     *      @OA\Parameter(
+     *          in="path",
+     *          name="keyword",
+     *          description="Keyword",
+     *          required=true,
+     *          @OA\Schema(
+     *              type="string",
+     *          ),
+     *      ),
+     *      @OA\Response(
+     *          response=400,
+     *          description="Invalid Token"
+     *      ),
+     *      @OA\Response(
+     *          response=401,
+     *          description="Token Expired"
+     *      ),
+     *      @OA\Response(
+     *          response=404,
+     *          description="Token Not Found"
+     *      ),
+     *      @OA\Response(
+     *          response=422,
+     *          description="Bad Request"
+     *      ),
+     *      @OA\Response(
+     *          response=500,
+     *          description="Internal Server Error"
+     *      ),
+     *      @OA\Response(
+     *          response=200,
+     *          description="Request OK"
+     *      )
+     * )
+     */
+    public function search( Request $request )
+    {
+
+        try {
+
+            $companies = $this->repository->search($request->get('keyword'))->all();
+
+        } catch(\Exception $e) {
+
+            return $this->apiErrorResponse(false, $e->getMessage(), self::INTERNAL_SERVER_ERROR, 'internalServerError');
+        }
+
+        return $this->apiSuccessResponse( compact( 'companies' ), true, '', self::HTTP_STATUS_REQUEST_OK);
+    }
+
     /**
      * @OA\Post(
      *      path="/company",
@@ -109,5 +177,20 @@ class ApiCompaniesController extends ApiBaseController
         }
 
         return $this->apiSuccessResponse( compact( 'company' ), true, 'Successfully registered a company', self::HTTP_STATUS_REQUEST_OK);
+    }
+
+    public function addJob( Request $request )
+    {
+
+    }
+
+    public function updateJob( Request $request )
+    {
+
+    }
+
+    public function deleteJob( Request $request )
+    {
+
     }
 }
