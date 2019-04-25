2c2625c7ad34533eb104881a207ce7d1857f4c73
remz <remz.fernandez@appetiser.com.au>
2019-04-25 19:09:15 +0800

    UPDATE skills update

--- app/Http/Controllers/API/V1/ApiUserSkillsController.php
+++ app/Http/Controllers/API/V1/ApiUserSkillsController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers\API\V1;
 use App\Repositories\UserRepository;
 use Illuminate\Http\Request;
 use JWTAuth;
-use App\Models\Users\UserSkill;
 
 class ApiUserSkillsController extends ApiBaseController
 {
@@ -18,87 +17,6 @@ class ApiUserSkillsController extends ApiBaseController
     public function __construct(UserRepository $repository) {
 
         $this->userRepo = $repository;
-    }
-
-    /**
-     * @OA\Post(
-     *      path="/user/skill",
-     *      tags={"User Skill"},
-     *      summary="Add a user skill",
-     *      security={{"BearerAuth":{}}},
-     *      @OA\RequestBody(
-     *          required=true,
-     *          @OA\MediaType(
-     *              mediaType="application/x-www-form-urlencoded",
-     *              @OA\Schema(
-     *                  type="object",
-     *                  @OA\Property(
-     *                      property="skill",
-     *                      description="<b>Required</b> Job Role",
-     *                      type="string",
-     *                      example="Team Effort"
-     *                  ),
-     *          @OA\Property(
-     *                      property="description",
-     *                      description="<b>Required</b> Company",
-     *                      type="string",
-     *                      example="A Team player with a passion."
-     *                  ),
-     *              ),
-     *          ),
-     *      ),
-     *      @OA\Response(
-     *          response=400,
-     *          description="Invalid Token"
-     *      ),
-     *      @OA\Response(
-     *          response=401,
-     *          description="Token Expired"
-     *      ),
-     *      @OA\Response(
-     *          response=404,
-     *          description="Token Not Found"
-     *      ),
-     *      @OA\Response(
-     *          response=422,
-     *          description="Bad Request"
-     *      ),
-     *      @OA\Response(
-     *          response=500,
-     *          description="Internal Server Error"
-     *      ),
-     *      @OA\Response(
-     *          response=200,
-     *          description="Request OK"
-     *      )
-     * )
-     */
-    public function add(Request $request)
-    {
-
-        $user = JWTAuth::toUser();
-        $skill = new UserSkill();
-        $skill->setUserId($user->id);
-
-        try {
-
-            if( !$skill->store($request) ){
-
-                return $this->apiErrorResponse(
-                    false,
-                    $skill->getErrors( true ),
-                    self::HTTP_STATUS_INVALID_INPUT,
-                    'invalidInput',
-                    $skill->getErrorsDetail()
-                );
-            }
-
-        } catch(\Exception $e) {
-
-            return $this->apiErrorResponse(false, $e->getMessage(), self::INTERNAL_SERVER_ERROR, 'internalServerError');
-        }
-
-        return $this->apiSuccessResponse( compact( 'skill' ), true, 'Successfully Added a Skill', self::HTTP_STATUS_REQUEST_OK);
     }
 
     /**
@@ -178,67 +96,4 @@ class ApiUserSkillsController extends ApiBaseController
         return $this->apiSuccessResponse( compact( 'skills' ), true, 'Successfully updated worker skills', self::HTTP_STATUS_REQUEST_OK);
     }
 
-
-    /**
-     * @OA\Delete(
-     *      path="/user/skill/{id}",
-     *      tags={"User Skill"},
-     *      summary="Delete a user skill",
-     *      security={{"BearerAuth":{}}},
-     *      @OA\Parameter(
-     *          in="path",
-     *          name="id",
-     *          description="Skill Id",
-     *          required=true,
-     *          @OA\Schema(
-     *              type="integer",
-     *          ),
-     *      ),
-     *      @OA\Response(
-     *          response=400,
-     *          description="Invalid Token"
-     *      ),
-     *      @OA\Response(
-     *          response=401,
-     *          description="Token Expired"
-     *      ),
-     *      @OA\Response(
-     *          response=404,
-     *          description="Token Not Found"
-     *      ),
-     *      @OA\Response(
-     *          response=422,
-     *          description="Bad Request"
-     *      ),
-     *      @OA\Response(
-     *          response=500,
-     *          description="Internal Server Error"
-     *      ),
-     *      @OA\Response(
-     *          response=200,
-     *          description="Request OK"
-     *      )
-     * )
-     */
-    public function delete(Request $request)
-    {
-
-        $skill = UserSkill::find($request->id);
-
-        if (!$skill) {
-
-            return $this->apiErrorResponse( false, 'Something wrong.', 400 , 'internalServerError' );
-        }
-
-        try {
-
-            $skill->delete();
-
-        } catch(\Exception $e) {
-
-            return $this->apiErrorResponse(false, $e->getMessage(), self::INTERNAL_SERVER_ERROR, 'internalServerError');
-        }
-
-        return $this->apiSuccessResponse( compact( 'skill' ), true, 'Successfully deleted a Skill', self::HTTP_STATUS_REQUEST_OK);
-    }
 }
