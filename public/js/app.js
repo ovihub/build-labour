30015c6c2c9b55ba86767fdef570893e797f4e02
Jamielyn Monique Oropesa <jam.oropesa@appetiser.com.au>
2019-05-01 10:41:28 +0800

    merge

--- public/js/app.js
+++ public/js/app.js
@@ -62990,6 +62990,8 @@ function () {
   }, {
     key: "setToken",
     value: function setToken(token) {
+      console.log('xxxxxxxxx');
+      console.log(this.tokenName);
       vue_cookie__WEBPACK_IMPORTED_MODULE_1___default.a.set(this.tokenName, JSON.stringify(token));
 
       this._invalidateToken();
@@ -65240,8 +65242,8 @@ window.Helper = {
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! D:\appetiser\build-labour-backend\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! D:\appetiser\build-labour-backend\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /Users/jamie/Documents/MyApps/appetiser/build-labour-backend/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /Users/jamie/Documents/MyApps/appetiser/build-labour-backend/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
