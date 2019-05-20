4ba78f87ae419ffe2bacf71a616062d5099cb316
remz <remz.fernandez@appetiser.com.au>
2019-05-20 18:02:25 +0800

    FIX admin login path

--- app/Http/Controllers/API/V1/Admin/ApiAuthController.php
+++ app/Http/Controllers/API/V1/Admin/ApiAuthController.php
@@ -14,47 +14,7 @@ use JWTAuth;
 class ApiAuthController extends ApiBaseController
 {
 
-    /**
-     * @OA\Post(
-     *      path="/auth/login",
-     *      tags={"Auth"},
-     *      summary="Login user. Authenticate credentials and returns a token.",
-     *      security={},
-     *      @OA\RequestBody(
-     *          required=true,
-     *          @OA\MediaType(
-     *              mediaType="application/x-www-form-urlencoded",
-     *              @OA\Schema(
-     *                  type="object",
-     *                  @OA\Property(
-     *                      property="email",
-     *                      description="Email Address",
-     *                      type="string",
-     *                      example="testuser@gmail.com"
-     *                  ),
-     *                  @OA\Property(
-     *                      property="password",
-     *                      description="Password",
-     *                      type="string",
-     *                      example="password"
-     *                  ),
-     *              ),
-     *          ),
-     *      ),
-     *      @OA\Response(
-     *          response=400,
-     *          description="Invalid Credentials"
-     *      ),
-     *      @OA\Response(
-     *          response=500,
-     *          description="Internal Server Error"
-     *      ),
-     *      @OA\Response(
-     *          response=200,
-     *          description="Login Success"
-     *      )
-     * )
-     */
+
     public function login( Request $request )
     {
         $credentials = $request->only('email', 'password');
