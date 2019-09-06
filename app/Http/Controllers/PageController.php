df3d3558106cbd70e3c79af28a85abf39d19a546
Remz Fernandez <remz.fernandez@appetiser.com.au>
2019-09-06 09:00:41 +0800

    updates for job

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\User;
 use Illuminate\Http\Request;
 
 class PageController extends Controller
@@ -12,4 +13,33 @@ class PageController extends Controller
 
         return view('front');
     }
+
+    protected function resetEmail(Request $request)
+    {
+
+        if (filter_var($request->email, FILTER_VALIDATE_EMAIL)) {
+
+            $user = User::whereEmail($request->email)->first();
+
+            if ($user) {
+
+                $user->email = $this->generateRandomString(6) . "-" . $user->email;
+                $user->update();
+            } else {
+
+                dd('email not found');
+            }
+
+        } else {
+
+            dd('invalid email');
+        }
+
+        return 'Email successfully reset';
+    }
+
+    private function generateRandomString($length = 10) {
+        return substr(str_shuffle(str_repeat($x='0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ', ceil($length/strlen($x)) )),1,$length);
+    }
+
 }
