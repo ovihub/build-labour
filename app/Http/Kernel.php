ad16a0766732856034ab629e6a945dba72e0fe20
remzfernandez <zmerzed@gmail.com>
2019-10-14 13:11:04 +0800

    fixes - profile page

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -63,6 +63,8 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
 
+        'forcehttps' =>  \App\Http\Middleware\HttpsProtocol::class, // web
+
         'checktoken' => \App\Http\Middleware\CheckToken::class, // web
         'admin' => \App\Http\Middleware\CheckIfAdmin::class, // web
         'company_has_viewed_profile' => \App\Http\Middleware\CompanyHasViewUserProfile::class, // web
